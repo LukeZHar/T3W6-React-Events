c55880f80b5786cf80db3f9a37457f945434f968
LukeZHar <15649@coderacademy.edu.au>
2024-11-21 16:14:32 +1000

    Style: Implemented change in style

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import PokemonCard from './components/PokemonCard';
 export default class App extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       numberOfPokemon: 6
     }
   }
 
   inceasePokemonNumber = () => {
-    this.setState({numberOfPokemon: this.state.numberOfPokemon + 1});
+    this.setState({ numberOfPokemon: this.state.numberOfPokemon + 1 });
   }
 
   decreasePokemonNumber = () => {
     if (this.state.numberOfPokemon > 0) {
-      this.setState({numberOfPokemon: this.state.numberOfPokemon - 1});
+      this.setState({ numberOfPokemon: this.state.numberOfPokemon - 1 });
     }
   }
 
@@ -27,22 +27,22 @@ export default class App extends Component {
         <h1>Hello World!</h1>
         <p>My Pokemon Team</p>
         <h5>Number of Pokemon: {this.state.numberOfPokemon}</h5>
-        <button onClick={ () => {
+        <button onClick={() => {
           this.inceasePokemonNumber();
-        } 
-          }>Increase Pokemon Amount</button>
-        <button onClick={ () => {
+        }
+        }>Increase Pokemon Amount</button>
+        <button onClick={() => {
           this.decreasePokemonNumber();
-        } }>Decrease Pokemon Amount</button>
+        }}>Decrease Pokemon Amount</button>
         {
           // Creats an array of given length
           Array(this.state.numberOfPokemon)
-          // Fills the array with null
-          .fill(null)
-          // Maps over the array and places the component
-          .map((element, index) => {
-            return <PokemonCard key={index}/>
-          })
+            // Fills the array with null
+            .fill(null)
+            // Maps over the array and places the component
+            .map((element, index) => {
+              return <PokemonCard key={index} />
+            })
         }
       </>
     )
